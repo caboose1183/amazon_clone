fb33e07366dbcbc7b5431ed6a8454255e614d8f6
Sidney Pang <sidneypang@hotmail.co.uk>
2023-04-30 14:54:38 -0400

    added checkout page for basket, including new total price selector

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -38,5 +38,7 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors, how we pull info from global store slice
 export const selectItems = (state) => state.basket.items;
+export const selectTotal = (state) =>
+  state.basket.items.reduce((total, item) => total + item.price, 0);
 
 export default basketSlice.reducer;
