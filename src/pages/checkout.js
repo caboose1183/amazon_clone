eafcaaa1d79ea99a108e37acf30ef3c29f3701b3
Sidney Pang <sidneypang@hotmail.co.uk>
2023-05-01 12:05:30 -0400

    Revert "added checkout page for basket, including new total price selector"
    This reverts commit fb33e07366dbcbc7b5431ed6a8454255e614d8f6.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,18 +1,12 @@
 import CheckoutProduct from "@/components/CheckoutProduct";
 import Header from "@/components/Header";
-import { selectItems, selectTotal } from "@/slices/basketSlice";
+import { selectItems } from "@/slices/basketSlice";
 import Image from "next/legacy/image";
 import React from "react";
 import { useSelector } from "react-redux";
 
-import Currency from "react-currency-formatter";
-import { useSession } from "next-auth/react";
-
 function Checkout() {
   const items = useSelector(selectItems);
-  const total = useSelector(selectTotal)
-;
-  const { data: session } = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -54,28 +48,7 @@ function Checkout() {
 
         {/* Right Side */}
 
-        <div className="flex flex-col bg-white p-10 shadow-md">
-          {items.length > 0 && (
-            <>
-              <h2 className="whitespace-nowrap">
-                Subtotal ({items.length} items) :{" "}
-                <span className="font-bold">
-                  <Currency quantity={total} currency="CAD" />
-                </span>
-              </h2>
-
-              <button
-                disabled={!session}
-                className={`button mt-2 ${
-                  !session &&
-                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
-                }`}
-              >
-                {!session ? "Sign in to checkout" : "Proceed to checkout"}
-              </button>
-            </>
-          )}
-        </div>
+        <div></div>
       </main>
     </div>
   );
